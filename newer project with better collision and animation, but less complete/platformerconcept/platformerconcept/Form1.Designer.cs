af11e9bb6fd15492571fed97e42a533b348f3bd6
Dan <42590607+slinksoft@users.noreply.github.com>
2020-02-20 13:21:23 -0500

    Add files via upload

---
+++ newer project with better collision and animation, but less complete/platformerconcept/platformerconcept/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace platformerconcept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.movementAndGravity = new System.Windows.Forms.Timer(this.components);
+            this.jump = new System.Windows.Forms.Timer(this.components);
+            this.mario = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.pf1 = new System.Windows.Forms.PictureBox();
+            this.pf2 = new System.Windows.Forms.PictureBox();
+            this.pf3 = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.coin3 = new System.Windows.Forms.PictureBox();
+            this.coin2 = new System.Windows.Forms.PictureBox();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.enemyMovement = new System.Windows.Forms.Timer(this.components);
+            this.goomba = new System.Windows.Forms.PictureBox();
+            this.pf4 = new System.Windows.Forms.PictureBox();
+            this.pf5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goomba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movementAndGravity
+            // 
+            this.movementAndGravity.Enabled = true;
+            this.movementAndGravity.Interval = 10;
+            this.movementAndGravity.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // jump
+            // 
+            this.jump.Enabled = true;
+            this.jump.Interval = 10;
+            this.jump.Tick += new System.EventHandler(this.Jump_Tick);
+            // 
+            // mario
+            // 
+            this.mario.BackColor = System.Drawing.Color.Transparent;
+            this.mario.Image = global::platformerconcept.Properties.Resources.littleidleright;
+            this.mario.Location = new System.Drawing.Point(192, 71);
+            this.mario.Name = "mario";
+            this.mario.Size = new System.Drawing.Size(27, 39);
+            this.mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mario.TabIndex = 2;
+            this.mario.TabStop = false;
+            // 
+            // floor
+            // 
+            this.floor.BackgroundImage = global::platformerconcept.Properties.Resources.mainfloor;
+            this.floor.Location = new System.Drawing.Point(-19, 365);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(695, 132);
+            this.floor.TabIndex = 3;
+            this.floor.TabStop = false;
+            // 
+            // pf1
+            // 
+            this.pf1.BackgroundImage = global::platformerconcept.Properties.Resources.brickblock;
+            this.pf1.Location = new System.Drawing.Point(509, 234);
+            this.pf1.Name = "pf1";
+            this.pf1.Size = new System.Drawing.Size(147, 38);
+            this.pf1.TabIndex = 4;
+            this.pf1.TabStop = false;
+            // 
+            // pf2
+            // 
+            this.pf2.BackgroundImage = global::platformerconcept.Properties.Resources.brickblock;
+            this.pf2.Location = new System.Drawing.Point(267, 94);
+            this.pf2.Name = "pf2";
+            this.pf2.Size = new System.Drawing.Size(147, 38);
+            this.pf2.TabIndex = 5;
+            this.pf2.TabStop = false;
+            // 
+            // pf3
+            // 
+            this.pf3.BackgroundImage = global::platformerconcept.Properties.Resources.brickblock;
+            this.pf3.Location = new System.Drawing.Point(12, 234);
+            this.pf3.Name = "pf3";
+            this.pf3.Size = new System.Drawing.Size(147, 38);
+            this.pf3.TabIndex = 6;
+            this.pf3.TabStop = false;
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.Color.White;
+            this.background.Image = global::platformerconcept.Properties.Resources.mainground;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(676, 433);
+            this.background.TabIndex = 7;
+            this.background.TabStop = false;
+            // 
+            // coin3
+            // 
+            this.coin3.BackColor = System.Drawing.Color.Transparent;
+            this.coin3.Image = global::platformerconcept.Properties.Resources.coin;
+            this.coin3.Location = new System.Drawing.Point(570, 172);
+            this.coin3.Name = "coin3";
+            this.coin3.Size = new System.Drawing.Size(21, 28);
+            this.coin3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin3.TabIndex = 8;
+            this.coin3.TabStop = false;
+            // 
+            // coin2
+            // 
+            this.coin2.BackColor = System.Drawing.Color.Transparent;
+            this.coin2.Image = global::platformerconcept.Properties.Resources.coin;
+            this.coin2.Location = new System.Drawing.Point(330, 12);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(21, 28);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin2.TabIndex = 9;
+            this.coin2.TabStop = false;
+            // 
+            // coin1
+            // 
+            this.coin1.BackColor = System.Drawing.Color.Transparent;
+            this.coin1.Image = global::platformerconcept.Properties.Resources.coin;
+            this.coin1.Location = new System.Drawing.Point(73, 172);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(21, 28);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin1.TabIndex = 10;
+            this.coin1.TabStop = false;
+            // 
+            // enemyMovement
+            // 
+            this.enemyMovement.Enabled = true;
+            this.enemyMovement.Interval = 50;
+            this.enemyMovement.Tick += new System.EventHandler(this.EnemyMovement_Tick);
+            // 
+            // goomba
+            // 
+            this.goomba.BackColor = System.Drawing.Color.Transparent;
+            this.goomba.Image = global::platformerconcept.Properties.Resources.Goomba;
+            this.goomba.Location = new System.Drawing.Point(100, 172);
+            this.goomba.Name = "goomba";
+            this.goomba.Size = new System.Drawing.Size(42, 46);
+            this.goomba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.goomba.TabIndex = 11;
+            this.goomba.TabStop = false;
+            // 
+            // pf4
+            // 
+            this.pf4.BackgroundImage = global::platformerconcept.Properties.Resources.brickblock;
+            this.pf4.Location = new System.Drawing.Point(-19, -25);
+            this.pf4.Name = "pf4";
+            this.pf4.Size = new System.Drawing.Size(36, 458);
+            this.pf4.TabIndex = 12;
+            this.pf4.TabStop = false;
+            // 
+            // pf5
+            // 
+            this.pf5.BackgroundImage = global::platformerconcept.Properties.Resources.brickblock;
+            this.pf5.Location = new System.Drawing.Point(648, 0);
+            this.pf5.Name = "pf5";
+            this.pf5.Size = new System.Drawing.Size(28, 433);
+            this.pf5.TabIndex = 13;
+            this.pf5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::platformerconcept.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(668, 425);
+            this.Controls.Add(this.goomba);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.coin2);
+            this.Controls.Add(this.coin3);
+            this.Controls.Add(this.pf2);
+            this.Controls.Add(this.mario);
+            this.Controls.Add(this.pf5);
+            this.Controls.Add(this.pf4);
+            this.Controls.Add(this.pf1);
+            this.Controls.Add(this.pf3);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.background);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(684, 464);
+            this.MinimumSize = new System.Drawing.Size(684, 464);
+            this.Name = "Form1";
+            this.Text = "2D Platformer Concept";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goomba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pf5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer movementAndGravity;
+        private System.Windows.Forms.Timer jump;
+        private System.Windows.Forms.PictureBox mario;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox pf1;
+        private System.Windows.Forms.PictureBox pf2;
+        private System.Windows.Forms.PictureBox pf3;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox coin3;
+        private System.Windows.Forms.PictureBox coin2;
+        private System.Windows.Forms.PictureBox coin1;
+        private System.Windows.Forms.Timer enemyMovement;
+        private System.Windows.Forms.PictureBox goomba;
+        private System.Windows.Forms.PictureBox pf4;
+        private System.Windows.Forms.PictureBox pf5;
+    }
+}
+
---
+++ newer project with better collision and animation, but less complete/platformerconcept/platformerconcept/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace platformerconcept
+{
+    public partial class Form1 : Form
+    {
+        bool left, right; // mario movement 
+        bool goLeft, goRight; // gooma movement
+        bool leftani, rightani; // mario directional animation
+        bool isJumping, onFloor; // checks for collision on floor
+        bool goomL, goomR, goomOnFloor; // gooma movement
+        int gravity = 5;
+        int moveSpeed = 3;
+        int velocity = 25;
+        int cVelocity = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // put primary controls in the background
+            mario.Parent = background;
+            goomba.Parent = background;
+            
+                    DoubleBuffered = true; // helps drawing
+            goomL = true; // set initial movement for goomba
+        }
+
+        private void EnemyMovement_Tick(object sender, EventArgs e)
+        {
+            goomOnFloor = false; // initial onfloor set
+            foreach (Control c in this.Controls)
+            {
+                // if intersects with any of these controls
+                if (c.Name == "pf1" || c.Name == "floor" || c.Name == "pf2" || c.Name == "pf3" || c.Name == "pf4" || c.Name == "pf5")
+                {
+                    if (goomba.Bounds.IntersectsWith(c.Bounds))
+                    {
+                        if (goomba.Right >= (c.Left) && goomba.Right <= (c.Left + 5) && goomba.Left <= c.Left) // touches right wall, turns left
+                        {
+                            goomR = false;
+                            goomL = true;
+                        }
+                        if (goomba.Left <= (c.Right) && goomba.Left >= (c.Right - 5) && goomba.Right >= c.Right) // touches left wall, turns right
+                        {
+                            goomL = false;
+                            goomR = true;
+                        }
+                        if (goomba.Bottom >= (c.Top) && goomba.Bottom <= (c.Top + 5)) // touches top surface, is on floor and don't fall down
+                        {
+                            goomOnFloor = true;
+                        }
+                        else // if not, fall down
+                        {
+                            goomOnFloor = false;
+                        }
+                    }
+                }
+            }
+
+            // actually pushes goomba down
+            if (goomOnFloor == false)
+            {
+                goomba.Top += gravity;
+            }
+
+            // moves gooma left or right
+            if (goomR == true)
+                goomba.Left += 2;
+            if (goomL == true)
+                goomba.Left -= 2;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // if pressing left arrow key, move left. movement happens in timer1
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+                goLeft = true;
+                goRight = false;
+                // set left animation
+                if (leftani == false && rightani == true && isJumping == false && onFloor == true)
+                {
+                    rightani = false;
+                    leftani = true;
+                    mario.Image = Properties.Resources.littleanileft;
+                }
+                else if (leftani == false && rightani == false && isJumping == false && onFloor == true)
+                {
+                    rightani = false;
+                    leftani = true;
+                    mario.Image = Properties.Resources.littleanileft;
+                }
+
+            }
+            // if pressing right arrow key, move right. movement happens in timer1
+            else if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+                goLeft = false;
+                goRight = true;
+                // set right animation
+                if (leftani == true && rightani == false && isJumping == false && onFloor == true)
+                {
+                    rightani = true;
+                    leftani = false;
+                    mario.Image = Properties.Resources.littleaniright;
+                }
+                else if (leftani == false && rightani == false && isJumping == false && onFloor == true)
+                {
+                    rightani = true;
+                    leftani = false;
+                    mario.Image = Properties.Resources.littleaniright;
+                }
+            }
+            // if pressing up arrow key, jump. movement happens in timer1
+            if (e.KeyCode == Keys.Up)
+            {
+                if (isJumping == false && onFloor == true)
+                {
+                    isJumping = true;
+ 
+                    cVelocity = velocity; // set jump velocity
+                    // set jump animation
+                    if (goLeft == true)
+                    {
+                        leftani = false;
+                        rightani = false;
+                        mario.Image = Properties.Resources.littlejumpleft;
+                    }
+                    else if (goRight == true)
+                    {
+                        leftani = false;
+                        rightani = false;
+                        mario.Image = Properties.Resources.littlejumpright;
+                    }
+                    jump.Start();
+                }
+            }
+
+            if (e.KeyCode == Keys.R)
+                Application.Restart();
+        }
+
+        private void Jump_Tick(object sender, EventArgs e)
+        {
+            // move mario up (jumping)
+            if (isJumping == true && cVelocity > 0)
+            {
+                mario.Top -= cVelocity;
+                cVelocity -= 1;
+            }
+            else
+            {
+                isJumping = false;
+                jump.Stop();
+            }
+        }
+
+        
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // stops moving left and sets idle animation
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+                if (leftani == true && isJumping == false && onFloor == true)
+                {
+                    mario.Image = Properties.Resources.littleidleleft;
+                    leftani = false;
+                    rightani = false;
+                }
+            }
+            //stops moving right and sets idle animation
+            else if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+                if (rightani == true && isJumping == false && onFloor == true)
+                {
+                    mario.Image = Properties.Resources.littleidleright;
+                    leftani = false;
+                    rightani = false;
+                }
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            onFloor = false; // initial onfloor set
+           
+
+            // if collides with any of these
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name == "pf1" || c.Name == "floor" || c.Name == "pf2" || c.Name == "pf3" || c.Name == "pf4" || c.Name == "pf5")
+                {
+                    if (mario.Bounds.IntersectsWith(c.Bounds))
+                    {
+                        if (mario.Right >= (c.Left) && mario.Right <= (c.Left + 5) && mario.Left <= c.Left) // cant move  right if hit left wall
+                            right = false;
+                         if (mario.Left <= (c.Right) && mario.Left >= (c.Right - 5) && mario.Right >= c.Right) // cant move left if it right wall
+                            left = false;
+
+                         if (mario.Top <= c.Bottom && mario.Bottom >= c.Bottom) // don't go through a bottom part of an object 
+                        {
+                            jump.Stop();
+                            isJumping = false;
+                         
+                        }
+                         if (mario.Bottom >= (c.Top) && mario.Bottom <= (c.Top + 5)) // if on top of platform, prevent gravity from pushing mario down
+                        {
+                            onFloor = true;
+                                if (goLeft == true && left == false)
+                                    mario.Image = Properties.Resources.littleidleleft;
+                                else if (goRight == true && right == false)
+                                    mario.Image = Properties.Resources.littleidleright;
+                        }
+                        else
+                        {
+                            onFloor = false;
+                        }
+                    }
+                }
+            }
+            // pushes mario down if not jumping or not on top of platform
+            if (onFloor == false && isJumping == false)
+            {
+                mario.Top += gravity;
+            }
+
+            // moves mario left or right
+            if (right == true)
+                mario.Left += moveSpeed;
+            if (left == true)
+                mario.Left -= moveSpeed;
+
+            // if collide with any coins, collect it
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name.Contains("coin"))
+                {
+                    if (mario.Bounds.IntersectsWith(c.Bounds))
+                    {
+                        c.Dispose();
+                    }
+                }
+            }
+
+            // if collides with goomba on top, kill it
+            if (mario.Bounds.IntersectsWith(goomba.Bounds))
+            {
+                if (mario.Bottom >= (goomba.Top) && mario.Top <= (goomba.Top))
+                {
+                    goomba.Dispose();
+                }
+            }
+        }
+    }
+
+}
